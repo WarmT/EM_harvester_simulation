e9b406bc9f32739a7131f0076703b4418e0e7cc3
Tero Kapanen <tero.kapanen@gmail.com>
2015-08-09 15:13:02 +0300

    First commit

---
+++ magnet_force.py
@@ -0,0 +1,64 @@
+from __future__ import division
+import numpy as np
+from numpy import cos, power, sqrt, pi, power, multiply, array
+from scipy.interpolate import interp1d
+from numba import jit
+
+@jit
+def calc_force_dist(m1_r, m1_t, m1_Br, m2_r, m2_t, m2_Br, h, Nr, Nphi):
+#    Nr = 20 
+#    Nphi = 60
+    mu0 = 1.256637e-6
+    m1_Ms = m1_Br/mu0          # 1.32 T / mu0
+    m2_Ms = m2_Br/mu0          # 1.32 T / mu0
+    m1_Rslice = (m1_r*m1_r)/Nr
+    m2_Rslice = (m2_r*m2_r)/Nr
+    const = mu0*m1_Ms*m2_Ms*Nphi/(4*pi)*multiply(pi*m1_Rslice/Nphi, pi*m2_Rslice/Nphi)
+
+    delta_phi = 2*pi/Nphi
+    hm = array([h, -(h+m1_t), -(h+m2_t), (h+m1_t+m2_t)], dtype='float')
+    S1 = 0.0
+    R_i_prev = 0.0
+    for i in range(0, Nr):
+        R_i = sqrt(R_i_prev*R_i_prev + m1_Rslice)
+        ri = (R_i+R_i_prev)/2
+        R_i_prev = R_i
+        S2 = 0.0
+        R_ii_prev = 0.0
+        for ii in range(0, Nr):
+            R_ii = sqrt(R_ii_prev*R_ii_prev + m2_Rslice)
+            rii = (R_ii+R_ii_prev)/2
+            R_ii_prev = R_ii
+            S3 = 0.0
+            ri_rii_producs = ri*ri+rii*rii
+            hm0_p1 = ri_rii_producs + hm[0]*hm[0]
+            hm1_p1 = ri_rii_producs + hm[1]*hm[1]
+            hm2_p1 = ri_rii_producs + hm[2]*hm[2]
+            hm3_p1 = ri_rii_producs + hm[3]*hm[3]
+            p2 = 2*ri*rii
+            for j in range(0, Nphi):
+                phi = j*delta_phi+delta_phi/2
+                S3 += hm[0]/power(hm0_p1-p2*cos(phi), 3/2) \
+                    + hm[1]/power(hm1_p1-p2*cos(phi), 3/2) \
+                    + hm[2]/power(hm2_p1-p2*cos(phi), 3/2) \
+                    + hm[3]/power(hm3_p1-p2*cos(phi), 3/2)
+            S2 += S3
+        S1 += S2
+    F = const * S1
+    return F
+
+def calc_force(m1_r, m1_t, m1_Br, m2_r, m2_t, m2_Br, h_range, Nr, Nphi):
+    F = np.zeros(h_range.size)
+    for index, h in enumerate(h_range):
+        F[index] = calc_force_dist(m1_r, m1_t, m1_Br, m2_r, m2_t, m2_Br, h, Nr, Nphi)
+    return F
+
+def calc_force_to_moving_magnet(m1_r, m1_t, m1_Br, m2_r, m2_t, m2_Br, Nr, Nphi, d0, h_steps):
+    h_min = 0.001 # minimum distance between opposing magnets is 1 mm
+    h_range = np.linspace(h_min, 2*d0, 2*h_steps)
+    F = calc_force(m1_r, m1_t, m1_Br, m2_r, m2_t, m2_Br, h_range, Nr, Nphi)
+    fem_int = interp1d(h_range, F, kind='cubic')
+    
+    x_int = np.linspace(-d0+2*h_min, d0-2*h_min, h_steps)
+    F_int = fem_int(d0-x_int)-fem_int(d0+x_int)
+    return (x_int, F_int)
---
+++ magnet_force_test.py
@@ -0,0 +1,58 @@
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from magnet_force import *
+import time
+from scipy.interpolate import interp1d
+
+
+def main():
+    m1_r = 0.009525/2
+    m1_t = 0.01905
+    m1_Br = 1.2
+    m2_r = 0.009525/2
+    m2_t = 0.003175
+    m2_Br = 1.2
+    Nr = 20
+    Nphi = 60
+    m = 0.01025
+    g = 9.81
+    
+    h_range = np.linspace(0.001, 0.062, 63)
+    F = calc_force(m1_r, m1_t, m1_Br, m2_r, m2_t, m2_Br, h_range, Nr, Nphi)
+    
+    fig = plt.figure(facecolor='white', figsize=(16, 9))
+#    plt.axhline(0, 0, 60, color='black')
+    plt.plot(np.multiply(h_range,1000), F, '-', lw=2, label='FEM')
+#    plt.axis([0.0, 60, -2.0, 6.0])
+    plt.xlabel('Separation Distance (mm)')
+    plt.ylabel('Repulsion Force (N)')
+    fig.set_tight_layout(True)
+    plt.show()
+    
+    h_steps = 65
+    fig = plt.figure(facecolor='white', figsize=(16, 9))
+    plt.axhline(0, -20, 20, color='black')
+    
+    d0_range = np.linspace(0.01, 0.03, 11)
+    for d0 in d0_range:
+        start = time.time()
+        x, F = calc_force_to_moving_magnet(m1_r, m1_t, m1_Br, m2_r, m2_t, m2_Br, Nr, Nphi, d0, h_steps)
+        end = time.time()
+        xe_int = interp1d(F, x, kind='cubic')
+        xe = xe_int(m*g)
+        print "d0 = %5.2f mm, xe = %6.3f mm" % (d0*1000, xe*1000)
+#        print "Force calculation takes %.3f seconds." % (end-start)
+        plt.plot(np.multiply(x,1000), F, '-', lw=2, label='d0 = %.2f mm' % (d0*1000))
+    
+    plt.legend(loc=2)
+    plt.axis([-30, 30, -10, 10])
+    plt.xlabel('Moving magnet position in harvester (mm)')
+    plt.ylabel('Repulsion Force (N)')
+    fig.set_tight_layout(True)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
